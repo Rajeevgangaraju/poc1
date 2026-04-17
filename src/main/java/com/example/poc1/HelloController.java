432aa279048edeb657e4fd4ad2ee6fa63b100407
Rajeevgangaraju <134952680+Rajeevgangaraju@users.noreply.github.com>
2026-04-17 12:18:52 +0530

    Create HelloController.java

---
+++ src/main/java/com/example/poc1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.poc1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Java + Docker + Jenkins ";
+    }
+}
